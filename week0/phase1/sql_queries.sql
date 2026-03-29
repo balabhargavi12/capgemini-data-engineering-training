6c23e3fce842f40dc387222f096df369e8ae1c2b
Golli Bala Bhargavi <balabhargavi28@gmail.com>
2026-03-30 02:22:58 +0530

    Update sql_queries.sql

--- week0/phase1/sql_queries.sql
+++ week0/phase1/sql_queries.sql
@@ -1,1 +1,14 @@
+-- 1. Show all customers
+SELECT * FROM customers;
 
+-- 2. Customers from Chennai
+SELECT * FROM customers WHERE city = 'Chennai';
+
+-- 3. Customers with age > 25
+SELECT * FROM customers WHERE age > 25;
+
+-- 4. Show name and city
+SELECT customer_name, city FROM customers;
+
+-- 5. Count customers city-wise
+SELECT city, COUNT(*) FROM customers GROUP BY city;
